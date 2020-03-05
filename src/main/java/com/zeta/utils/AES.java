b409b0393e3819b03acbc991eeaffb365d587e66
Zeta <kangpele@gmail.com>
2020-03-05 17:40:50 +0900

    AWS KMS이용 유틸

---
+++ src/main/java/com/zeta/utils/AES.java
@@ -0,0 +1,76 @@
+package com.zeta.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * AES 알고리즘 유틸리티
+ * @author zeta
+ *
+ */
+public class AES {
+
+	private static SecretKeySpec secretKey;
+	private static byte[] key;
+
+	/**
+	 * seed key세팅
+	 * @param myKey
+	 */
+	public static void setKey(String myKey) {
+		MessageDigest sha = null;
+		try {
+			key = myKey.getBytes("UTF-8");
+			sha = MessageDigest.getInstance("SHA-1");
+			key = sha.digest(key);
+			key = Arrays.copyOf(key, 16);
+			secretKey = new SecretKeySpec(key, "AES");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 암호화
+	 * @param strToEncrypt
+	 * @param secret
+	 * @return
+	 */
+	public static String encrypt(String strToEncrypt, String secret) {
+		try {
+			setKey(secret);
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+		} catch (Exception e) {
+			System.out.println("Error while encrypting: " + e.toString());
+		}
+		return null;
+	}
+    
+	/**
+	 * 복호화
+	 * @param strToDecrypt
+	 * @param secret
+	 * @return
+	 */
+	public static String decrypt(String strToDecrypt, String secret) {
+		try {
+			setKey(secret);
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+			return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+		} catch (Exception e) {
+			System.out.println("Error while decrypting: " + e.toString());
+		}
+		return null;
+	}
+}
